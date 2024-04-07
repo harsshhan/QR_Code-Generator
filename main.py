ae0f1979a4f62d20ae6d604eada2129104948f6b
HARSHAN A M <harshanmathi06@gmail.com>
2024-04-07 18:52:22 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+import qrcode
+qr = qrcode.QRCode(border=1.5)
+qr.add_data(input('Enter url: '))
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save('qr.png')
